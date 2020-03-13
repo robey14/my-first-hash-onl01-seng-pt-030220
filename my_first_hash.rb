896f4f3f9d1b01cc35582b6225ab05b7d80120ed
John Michael Robey <robey14@gmail.com>
2020-03-13 15:39:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,7 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest = {"muskets" => 2, "gun powder" => 4}
 end
   shipping_manifest["pearl necklace"]=1
   shipping_manifest
