fa3250ff9c32a630c9fc6bf4b66c6aa5845b535d
John Michael Robey <robey14@gmail.com>
2020-03-13 15:48:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,8 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {"muskets" => 2, "gun powder" => 4}
+  shipping_manifest[muskets] = 2
+  shipping_manifest[gun powder] = 4{"muskets" => 2, "gun powder" => 4}
 end
   shipping_manifest["pearl necklace"]=1
   shipping_manifest
